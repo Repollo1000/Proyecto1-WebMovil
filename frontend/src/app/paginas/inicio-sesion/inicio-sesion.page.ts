a3a89c72bf10c1088c78d36336f6dae30bf3e168
Projas-14 <paul.rojas.o@mail.pucv.cl>
2024-05-24 23:09:34 -0400

    aa
    aaa

---
+++ frontend/src/app/paginas/inicio-sesion/inicio-sesion.page.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router'; // Importa el Router
+
+@Component({
+  selector: 'app-inicio-sesion',
+  templateUrl: './inicio-sesion.page.html',
+  styleUrls: ['./inicio-sesion.page.scss'],
+})
+export class InicioSesionPage implements OnInit {
+  email: string = '';
+  password: string = '';
+
+  constructor(private alertController: AlertController, private http: HttpClient, private router: Router) { } // Añade Router al constructor
+
+  ngOnInit() { }
+
+  async login() {
+    if (!this.email || !this.password) {
+      await this.showAlert('Error', 'Todos los campos son obligatorios.');
+      return;
+    }
+
+    this.http.post('http://localhost:3000/login', { email: this.email, password: this.password }).subscribe(
+      response => {
+        // Redirigir al dashboard después de un inicio de sesión exitoso
+        this.router.navigate(['/info']);
+      },
+      async error => {
+        await this.showAlert('Error', error.error.message || 'Ocurrió un error al procesar la solicitud');
+      }
+    );
+  }
+
+  async showAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header,
+      message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+}
