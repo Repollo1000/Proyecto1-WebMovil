342b8acbca381a0cb55e4fa3b144e4350bb90367
Projas-14 <paul.rojas.o@mail.pucv.cl>
2024-05-26 14:09:32 -0400

    Actualizacion footer
    Footer - seleccionar segun la pagina

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent implements OnInit {
+  currentRoute: string = ''; // Inicializar currentRoute con una cadena vacía
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.urlAfterRedirects;
+      }
+    });
+  }
+
+  ngOnInit() {}
+}
